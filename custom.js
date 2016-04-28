5b462d2d3d104f87b70cf1f62aa7940bdd7b602b
Daniel Alzugaray <dalzuga@gmail.com>
2016-04-28 12:02:00 -0700

    for loop constructed to handle additional star inputs

--- custom.js
+++ custom.js
@@ -10,4 +10,11 @@ $(document).ready(function() {
         $("#input-1").on('rating.change', function(event, value, caption) {
             alert("New value is " + value);
         });
+
+        for (var i = 2; i < 3; i++) {
+                $("#input-" + i).rating({min:0, max:3, step:.5, size:'xs', stars:3});
+                $("#input-" + i).on('rating.change', function(event, value, caption) {
+                        alert("New value is " + value);
+                })
+        }
 });
